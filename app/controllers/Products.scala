d8f58e641afe9e2e8e6705bec918f17c6b6d0565
Erdong Feng <edfeng@thoughtworks.com>
2014-04-16 22:44:23 +0800

    enable check product details.

--- app/controllers/Products.scala
+++ app/controllers/Products.scala
@@ -7,4 +7,12 @@ object Products extends Controller {
   def list = Action {
     Ok(views.html.products.list(Product.findAll))
   }
+
+  def details(ean: Long) = Action {
+    val product = Product.findByEan(ean)
+    product match {
+      case Some(product) => Ok(views.html.products.details(product))
+      case None => Ok(views.html.products.notFound())
+    }
+  }
 }
