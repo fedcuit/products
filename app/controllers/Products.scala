240edea2f9790eaed86abe50d6bbf7eafb677f63
Erdong Feng <edfeng@thoughtworks.com>
2014-04-16 21:55:12 +0800

    show product list page with test data.

--- app/controllers/Products.scala
+++ app/controllers/Products.scala
@@ -1,5 +1,10 @@
 package controllers
 
-class Products {
+import play.api.mvc.{Action, Controller}
+import models.Product
 
+object Products extends Controller {
+  def list = Action {
+    Ok(views.html.products.list(Product.findAll))
+  }
 }
