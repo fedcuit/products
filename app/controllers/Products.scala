22d53cbeb18d94311c75d6cdda52640d2de1acda
Erdong Feng <edfeng@thoughtworks.com>
2014-04-17 20:28:51 +0800

    add Form object to map between http entity to product object.

--- app/controllers/Products.scala
+++ app/controllers/Products.scala
@@ -1,9 +1,20 @@
 package controllers
 
 import play.api.mvc.{Action, Controller}
+import play.api.data._
+import play.api.data.Forms._
 import models.Product
 
 object Products extends Controller {
+  private val productForm: Form[Product] = Form(
+    mapping(
+      "ean" -> longNumber.verifying(
+        "validation.ean.duplicate", Product.findByEan(_).isEmpty),
+      "name" -> nonEmptyText,
+      "description" -> nonEmptyText
+    )(Product.apply)(Product.unapply)
+  )
+
   def list = Action {
     Ok(views.html.products.list(Product.findAll))
   }
