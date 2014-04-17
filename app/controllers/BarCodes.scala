8f30977b8dddc0ffa1309162871a52102e83a16e
Erdong Feng <edfeng@thoughtworks.com>
2014-04-17 16:25:24 +0800

    add controller to generate barcode

---
+++ app/controllers/BarCodes.scala
@@ -0,0 +1,26 @@
+package controllers
+
+import play.api.mvc.{Action, Controller}
+
+object BarCodes extends Controller {
+  def barcode(ean: Long) = Action {
+    val MimeType = "image/png"
+    Ok(ean13BarCode(ean, MimeType)).as(MimeType)
+  }
+
+  def ean13BarCode(ean: Long, mimeType: String): Array[Byte] = {
+    import java.io.ByteArrayOutputStream
+    import java.awt.image.BufferedImage
+    import org.krysalis.barcode4j.output.bitmap.BitmapCanvasProvider
+    import org.krysalis.barcode4j.impl.upcean.EAN13Bean
+
+    val output: ByteArrayOutputStream = new ByteArrayOutputStream
+    val canvas: BitmapCanvasProvider =
+      new BitmapCanvasProvider(output, mimeType, 100,
+        BufferedImage.TYPE_BYTE_BINARY, false, 0)
+    val barcode = new EAN13Bean()
+    barcode.generateBarcode(canvas, String valueOf ean)
+    canvas.finish()
+    output.toByteArray
+  }
+}
--- build.sbt
+++ build.sbt
@@ -6,6 +6,10 @@ libraryDependencies ++= Seq(
   jdbc,
   anorm,
   cache
-)     
+)
+
+libraryDependencies ++= Seq(
+  "net.sf.barcode4j" % "barcode4j" % "2.0"
+)
 
 play.Project.playScalaSettings
