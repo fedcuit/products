494c4774ea394ee294248409bf08d045438bab3c
Erdong Feng <edfeng@thoughtworks.com>
2014-04-28 22:22:06 +0800

    controller logic to handle edit a product.

--- app/controllers/Products.scala
+++ app/controllers/Products.scala
@@ -6,10 +6,18 @@ import play.api.data.Forms._
 import models.Product
 
 object Products extends Controller {
-  private val productForm: Form[Product] = Form(
+  private val newForm: Form[Product] = Form(
     mapping(
       "ean" -> longNumber.verifying(
         "validation.ean.duplicate", Product.findByEan(_).isEmpty),
+      "name" -> nonEmptyText,
+      "description" -> nonEmptyText
+    )(Product.apply)(Product.unapply)
+  )
+
+  private val editForm: Form[Product] = Form(
+    mapping(
+      "ean" -> longNumber.verifying("validation.ean.doesnotexist", Product.findByEan(_).isDefined),
       "name" -> nonEmptyText,
       "description" -> nonEmptyText
     )(Product.apply)(Product.unapply)
@@ -29,14 +37,14 @@ object Products extends Controller {
 
   def newProduct = Action {
     implicit request =>
-      val form = if (flash.get("error").isDefined) productForm.bind(flash.data) else productForm
+      val form = if (flash.get("error").isDefined) newForm.bind(flash.data) else newForm
       Ok(views.html.products.newProduct(form))
   }
 
   def save = Action {
     implicit request =>
-      val newProductForm: Form[Product] = productForm.bindFromRequest()
-      newProductForm.fold(
+      val form: Form[Product] = newForm.bindFromRequest()
+      form.fold(
         hasErrors = {
           form =>
             Redirect(routes.Products.newProduct()).flashing(Flash(form.data) + "error" -> "Please check your input")
@@ -45,6 +53,34 @@ object Products extends Controller {
           newProduct =>
             Product.add(newProduct)
             Redirect(routes.Products.details(newProduct.ean)).flashing("success" -> "New product added successfully")
+        }
+      )
+  }
+
+  def editProduct(ean: Long) = Action {
+    implicit request =>
+      val product: Option[Product] = Product.findByEan(ean)
+      product match {
+        case Some(p) => {
+          val form: Form[Product] = if (flash.get("error").isDefined) newForm.bind(flash.data) else newForm.fill(p)
+          Ok(views.html.products.editProduct(form))
+        }
+        case None => NotFound
+      }
+  }
+
+  def update(ean: Long) = Action {
+    implicit request =>
+      val form: Form[Product] = editForm.bindFromRequest()
+      form.fold(
+        hasErrors = {
+          form =>
+            Redirect(routes.Products.editProduct(ean)).flashing(Flash(form.data) + "error" -> form.errorsAsJson.toString())
+        },
+        success = {
+          product =>
+            Product.update(product.copy())
+            Redirect(routes.Products.details(ean)).flashing("success" -> "Product updated successfully")
         }
       )
   }
