d138aa75497a276f1a17190958ce36ea7e641448
Erdong Feng <edfeng@thoughtworks.com>
2014-04-28 23:02:24 +0800

    front end work for removing a product.

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,3 +1,16 @@
-/**
- * Created by edfeng on 4/28/14.
- */
+$(function () {
+    $('.btn-danger').click(function(){
+        var self = $(this);
+        var url = self.data().url;
+        $.ajax({
+            method: 'DELETE',
+            url: url,
+            success: function() {
+                self.parents('li').remove();
+            },
+            error: function() {
+                console.log('error');
+            }
+        });
+    });
+});
